67940c472b5fe38599600d19672e495a8ffbda76
robert1003 <b07902047@ntu.edu.tw>
2020-05-24 00:40:10 +0800

    0.71650

--- hw12_test.sh
+++ hw12_test.sh
@@ -1,5 +1,5 @@
 data_directory=$1
 prediction_file=$2
-model_name=raw_adap.pth
+model_name=raw_adap_400.pth
 
 python3 hw12_test.py --model_checkpoint $model_name --dataroot $data_directory --output_csv $prediction_file
