37b761e7df08d5d603c33ee54c544cef1bb50653
robert1003 <b07902047@ntu.edu.tw>
2020-05-27 15:30:52 +0800

    add new model files and script

--- hw12_test.sh
+++ hw12_test.sh
@@ -1,5 +1,6 @@
 data_directory=$1
 prediction_file=$2
-model_name=raw_adap_400.pth
 
-python3 hw12_test.py --model_checkpoint $model_name --dataroot $data_directory --output_csv $prediction_file --use_gpu
+python3 hw12_test.py --dataroot $data_directory --output_csv $prediction_file --use_gpu\
+  --model_checkpoint resnet_18_adap_2500.pth resnet_34_adap_2000.pth resnet_18_adap_2000.pth\
+  --resnet_type 18 34 18
